8dbdbf7dccd4b76f42ab84c9bb874f34745dbd9d
Faruk Abdulla Munshi <informfaruk@gmail.com>
2020-03-12 21:32:35 +0530

    add user routes at index.js

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,45 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+/* GET /forgot-pw */
+router.get('/forgot-pw', (req, res, next) => {
+  res.send('GET /forgot-pw');
+});
+/* PUT /forgot-pw */
+router.put('/forgot-pw', (req, res, next) => {
+  res.send('PUT /forgot-pw');
+});
+/* GET /reset-pw */
+router.get('/reset-pw/:token', (req, res, next) => {
+  res.send('GET /reset-pw/:token');
+});
+/* GET /reset-pw */
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('PUT /reset-pw/:token');
+});
 
 module.exports = router;
